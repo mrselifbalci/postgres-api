6ca41070174b0ed620f7db208734b770b00925b5
Elif Balci <mrselifbalci@gmail.com>
2022-02-27 15:51:48 +0000

    .

--- connection.js
+++ connection.js
@@ -1,11 +1,11 @@
 const {Client} = require('pg')
 
 const client = new Client({
-    host: "localhost",
-    user: "postgres",
+    host: "ec2-54-228-162-209.eu-west-1.compute.amazonaws.com",
+    user: "exjbpykxzuczsv",
     port: 5432,
-    password: "London2020%",
-    database: "postgres"
+    password: "64f264bb81c539bb5c7a253a38db44ed46a1676a79618db8280d79661fa9f523",
+    database: "dd0qo3bi0gum0q"
 })
 
 module.exports = client;
